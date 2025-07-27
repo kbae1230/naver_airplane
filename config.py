6b350f44c29e23a556cfc23a741df091af50f6be
hyungkunbae <76417472+kbae1230@users.noreply.github.com>
2025-07-27 10:52:13 +0000

    add os path

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 DATABASE_ID = os.getenv("DATABASE_ID")
 PAGE_ID = os.getenv("PAGE_ID")
 NOTION_VERSION = "2022-06-28"
-DATA_PATH = "/home/kbae/naver_airplane/data.json"
+DATA_PATH = os.path.join(BASE_DIR, "data.json")
 AIRPORT = ["CJU", "GMP", "PUS", "ICN", "CJJ"]
